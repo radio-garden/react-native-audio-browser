2701671cbe6e73a797c396c2008b14806d201d01
puckey <jonathan@radio.garden>
2025-10-12 17:26:59 +0200

    style(android): apply ktfmt formatting to Kotlin files
    - Format HybridReactNativeAudioBrowser.kt with proper spacing and indentation
    - Convert ReactNativeAudioBrowserPackage.kt to Kotlin style formatting
    - Remove Java-style semicolons and apply consistent code style

--- android/src/main/java/com/margelo/nitro/reactnativeaudiobrowser/HybridReactNativeAudioBrowser.kt
+++ android/src/main/java/com/margelo/nitro/reactnativeaudiobrowser/HybridReactNativeAudioBrowser.kt
@@ -1,7 +1,7 @@
 package com.margelo.nitro.reactnativeaudiobrowser
 
-class HybridReactNativeAudioBrowser: HybridReactNativeAudioBrowserSpec() {
-    override fun sum(num1: Double, num2: Double): Double {
-        return num1 + num2
-    }
+class HybridReactNativeAudioBrowser : HybridReactNativeAudioBrowserSpec() {
+  override fun sum(num1: Double, num2: Double): Double {
+    return num1 + num2
+  }
 }
--- android/src/main/java/com/reactnativeaudiobrowser/ReactNativeAudioBrowserPackage.kt
+++ android/src/main/java/com/reactnativeaudiobrowser/ReactNativeAudioBrowserPackage.kt
@@ -1,20 +1,21 @@
-package com.reactnativeaudiobrowser;
+package com.reactnativeaudiobrowser
 
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.module.model.ReactModuleInfoProvider;
-import com.facebook.react.TurboReactPackage;
-import com.margelo.nitro.reactnativeaudiobrowser.ReactNativeAudioBrowserOnLoad;
-
+import com.facebook.react.TurboReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.margelo.nitro.reactnativeaudiobrowser.ReactNativeAudioBrowserOnLoad
 
 public class ReactNativeAudioBrowserPackage : TurboReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? = null
 
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider = ReactModuleInfoProvider { emptyMap() }
+  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider = ReactModuleInfoProvider {
+    emptyMap()
+  }
 
   companion object {
     init {
-      ReactNativeAudioBrowserOnLoad.initializeNative();
+      ReactNativeAudioBrowserOnLoad.initializeNative()
     }
   }
 }
