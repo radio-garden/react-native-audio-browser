592cfd91bde5d60fb94ad38869df38cb16c2a1a6
puckey <jonathan@radio.garden>
2025-10-12 16:40:52 +0200

    fix(android): move HybridReactNativeAudioBrowser to correct Nitro package
    Move HybridReactNativeAudioBrowser.kt from com.reactnativeaudiobrowser to
    com.margelo.nitro.reactnativeaudiobrowser package to match Nitro's expected
    class location. This resolves the runtime error where NitroModulesProxy
    couldn't find the hybrid object class.

--- android/src/main/java/com/reactnativeaudiobrowser/HybridReactNativeAudioBrowser.kt
+++
@@ -1,9 +0,0 @@
-package com.reactnativeaudiobrowser
-
-import com.margelo.nitro.reactnativeaudiobrowser.HybridReactNativeAudioBrowserSpec
-
-class HybridReactNativeAudioBrowser: HybridReactNativeAudioBrowserSpec() {    
-    override fun sum(num1: Double, num2: Double): Double {
-        return num1 + num2
-    }
-}
